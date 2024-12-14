5b32d5fd9fe13c36042b97b09dbbd3c7b892a766
Pushpanjali Adat <pushpanjaliadat15@gmail.com>
2024-12-14 12:30:32 +0530

    made changes in ArrayListExample test cases to include reporting

---
+++ ArrayListExample.java
@@ -0,0 +1,22 @@
+package Practice;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Object> a3=new ArrayList<Object>();
+		a3.add("Pushpanjali");
+		a3.add("Rutuja");
+		a3.add("Shivani");
+		a3.add(123);
+		a3.add(24.1);
+		a3.add(10000000);
+		a3.add(12.4f);
+		System.out.println(a3);
+		
+		
+	}
+
+}
